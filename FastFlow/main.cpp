deedc2fd29dcb2d4f40735377d26df318bd4a88d
Maxim Grishkin <grishkin.max@gmail.com>
2015-11-02 18:02:25 +0300

    Initial Commit

---
+++ FastFlow/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FastFlow
+//
+//  Created by Maxim Grishkin on 02/11/15.
+//  Copyright © 2015 Maxim Grishkin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
